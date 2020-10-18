1ad6b5dd9b96ee5893da663134d343ff758c79a2
bandrewv <brock.vriesman@gmail.com>
2020-10-18 12:59:44 -0700

    Chart plotted, working on circle labels.

--- D3-Data-Journalism/assets/js/app.js
+++ D3-Data-Journalism/assets/js/app.js
@@ -1,13 +1,13 @@
 // @TODO: YOUR CODE HERE!
 
 // Set dimensions and margins of the graph
-var svgWidth = 960;
-var svgHeight = 600;
+var svgWidth = 750;
+var svgHeight = 700;
 
 var margin = {
     top: 60,
     right: 60,
-    bottom: 60,
+    bottom: 90,
     left: 60
 };
 
@@ -26,94 +26,64 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Load data from data.csv
-d3.csv("assets/data/data.csv").then(data => {
+d3.csv("assets/data/data.csv").then(healthData => {
 
     // Print the data
-    console.log(data);
-  
-    // Cast the poverty and smokes values to numbers
-    data.forEach(function(state) {
-      state.poverty = +state.poverty;
-      state.smokes = +state.smokes;
-      state.obesity = +state.obesity;
+    console.log(healthData);
+    
+    stateAbbr = [];
+
+    // Cast the poverty and obesity values to numbers
+    healthData.forEach(data => {
+        data.poverty = +data.poverty;
+        data.obesity = +data.obesity;
+        stateAbbr.push(data.abbr);
     });
     
-
     // Configure a linear scale with a range between the chartHeight and 0
-    var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(data, ypoint => ypoint.poverty)])
-    .range([chartHeight, 0]);
 
     var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(data, xpoint => xpoint.obesity)])
-    .range([0, chartWidth]);
+    .domain([d3.min(healthData, d => d.obesity), d3.max(healthData, d => d.obesity)])
+    .range([0, chartWidth]).nice();
 
+    var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(healthData, d => d.poverty), d3.max(healthData, d => d.poverty)])
+    .range([chartHeight, 0]).nice();
 
     // Create two new functions passing the scales in as arguments
     // These will be used to create the chart's axes
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-
-    var drawLine = d3.line()
-    .x(xpoint => xLinearScale(xpoint.obesity))
-    .y(ypoint => yLinearScale(ypoint.poverty));
-
-    // Append an SVG path and plot its points using the line function
-    chartGroup.append("path")
-    // The drawLine function returns the instructions for creating the line for forceData
-    .attr("d", drawLine(data))
-    .classed("line", true);
-
-
-    // Append an SVG group element to the chartGroup, create the left axis inside of it
+    // Append the "g" elements to the chartGroup, creating axes
     chartGroup.append("g")
-        .classed("axis", true)
-        .call(leftAxis);
-
-    // Append an SVG group element to the chartGroup, create the bottom axis inside of it
-    // Translate the bottom axis to the bottom of the page
-    chartGroup.append("g")
-        .classed("axis", true)
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(bottomAxis);
+    
+    chartGroup.append("g")
+        .call(leftAxis);
 
+    // Append scatter plot circles to the chart
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
+        .join("circle")
+        .attr("cx", d => xLinearScale(d.obesity))
+        .attr("cy", d => yLinearScale(d.poverty))
+        .attr("r", 7.5)
+        .attr("fill", "blue")
+        .text(d => d.abbr)
 
-    console.log(yLinearScale);
-
-    //   // Append an SVG group element to the chartGroup, create the left axis inside of it
-    // chartGroup.append("g")
-    // .classed("axis", true)
-    // .call(leftAxis);
-
-    // // Append an SVG group element to the chartGroup, create the bottom axis inside of it
-    // // Translate the bottom axis to the bottom of the page
-    // chartGroup.append("g")
-    // .classed("axis", true)
-    // .attr("transform", `translate(0, ${chartHeight})`)
-    // .call(bottomAxis);
-
-
-    // Configure a smoking scale
-    // var smokeTimeScale = d3.
-
-    // var barSpacing = 10; // desired space between each bar
-    // var scaleY = 8; // 8x scale on rect height
-  
-    // // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-    // var barWidth = (chartWidth - (barSpacing * (tvData.length - 1))) / tvData.length;
-  
-    // // @TODO
-    // // Create code to build the bar chart using the tvData.
-    // chartGroup.selectAll(".bar")
-    //   .data(tvData)
-    //   .join("rect")
-    //   .classed("bar", true)
-    //   .attr("width", d => barWidth)
-    //   .attr("height", d => d.hours * scaleY)
-    //   .attr("x", (d, i) => i * (barWidth + barSpacing))
-    //   .attr("y", d => chartHeight - d.hours * scaleY);
-  
-//   }).catch(error => console.log(error));
-
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Poverty Rate");
+    
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top})`)
+        .attr("class", "axisText")
+        .text("Obesity Rate");
 });
